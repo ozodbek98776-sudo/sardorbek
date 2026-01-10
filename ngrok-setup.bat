d349fb92b397baa46d1c58fabe5cf2b06c43912f
alisher namozov <alishernamozov286@gmail.com>
2026-01-10 14:37:32 +0500

    Clean up deleted files after merge

--- ngrok-setup.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Ngrok orqali telefondan kirish...
-echo.
-
-echo Frontend uchun tunnel ochilmoqda...
-echo Telefondan berilgan URL ga kiring
-echo.
-
-ngrok http 5173
--- tunnel-start.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Sardor Furnitura Tunnel ishga tushirilmoqda...
-echo.
-
-REM Tunnel ishga tushirish
-cloudflared tunnel --config config.yml run sardor-furnitura
-
-pause
